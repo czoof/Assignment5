57d21b30c1cf63da42899fec2009144c8f30c543
czoof <charliezufan15z@gmail.com>
2025-11-01 21:56:07 -0500

    Fixed Error

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,4 @@
 export default {
-  testEnvironment: "node",
-  transform: {},
-  setupFilesAfterEnv: ["<rootDir>/test/setup.js"]
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
 };
